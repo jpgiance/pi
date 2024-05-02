61a36d88f2a5a9152cef4346de9d8d376d93f51f
Jorge <jpgiance@gmail.com>
2024-05-02 13:33:18 -0500

    adding main script

---
+++ zmq/main.py
@@ -0,0 +1,20 @@
+import subprocess
+import time
+
+def start_uart_server():
+    subprocess.Popen(["python", "uart.py"])
+
+def start_usb_host_process():
+    subprocess.Popen(["sudo", "python", "android2.py"])
+
+def start_bluetoothLE_process():
+    subprocess.Popen(["python", "bluetooth2.py"])
+
+
+# Start the components in the desired order
+start_uart_server()
+time.sleep(0.5)  # Wait for the server to start
+start_usb_host_process()
+start_bluetoothLE_process()
+
+# Continue with any other necessary startup tasks
