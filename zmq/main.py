edddbe4fbd7ac18cc86e88aa68b49e1e580a94a0
Jorge <jpgiance@gmail.com>
2024-05-02 13:45:13 -0500

    adding feedback to process  creation

--- zmq/main.py
+++ zmq/main.py
@@ -1,14 +1,25 @@
 import subprocess
 import time
 
+def start_process(command, name):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    if process.returncode == 0:
+        print(f"{name} started successfully.")
+    else:
+        print(f"Failed to start {name}.")
+        print(f"stdout: {stdout.decode('utf-8')}")
+        print(f"stderr: {stderr.decode('utf-8')}")
+
 def start_uart_server():
-    subprocess.Popen(["python", "uart.py"])
+    start_process(["python", "uart.py"], "uart")
 
 def start_usb_host_process():
-    subprocess.Popen(["sudo", "python", "android2.py"])
+    start_process(["sudo", "python", "android2.py"], "usb_host")
 
 def start_bluetoothLE_process():
-    subprocess.Popen(["python", "bluetooth2.py"])
+    start_process(["python", "bluetooth2.py"], "bluetoothLE")
 
 
 # Start the components in the desired order
